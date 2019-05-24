8afa48665352b05474b562beb77c2df792a39fc1
Rafael Musmanno <rafa.musmanno@gmail.com>
2019-05-24 14:18:28 -0300

    Adicionando campos ao modelo

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from model.BankStatement import BankStatement
 from model.CashFlow import CashFlow, CashFlowType
+import datetime
 
-outflow = [CashFlow(CashFlowType.DEBIT, -100.0),
-            CashFlow(CashFlowType.DEBIT, -127.0),
-            CashFlow(CashFlowType.CREDIT, -42.0)]
+outflow = [CashFlow('debit 1', CashFlowType.DEBIT, -100.0, datetime.datetime.now()),
+            CashFlow('debit 2', CashFlowType.DEBIT, -127.0),
+            CashFlow('credit 1', CashFlowType.CREDIT, -42.0)]
 
-inflow = [CashFlow(CashFlowType.CREDIT, 1000.0)]
+inflow = [CashFlow('credit 1', CashFlowType.CREDIT, 1000.0)]
 
 bs = BankStatement(inflow, outflow)
 
--- model/CashFlow.py
+++ model/CashFlow.py
@@ -1,15 +1,27 @@
 from enum import Enum
+from datetime import datetime
 
 class CashFlowType(Enum):
     DEBIT = 1
     CREDIT = 2
 
 class CashFlow:
-    def __init__(self, 
-                flowType: CashFlowType = CashFlowType.DEBIT, 
-                value: float = 0.0):
+    def __init__(self,
+                name = 'N/A',
+                flowType: CashFlowType = CashFlowType.DEBIT,
+                value: float = 0.0,
+                date: datetime = 'N/A'):
+        self.name = name
         self.flowType = flowType
         self.value = value
+        self.date = date
 
     def __repr__(self):
-        return 'CashFlow:\n\tType: {}\n\tValue: {}'.format(self.flowType.name, self.value)
+        return 'CashFlow: \
+                \n\tName: {} \
+                \n\tType: {} \
+                \n\tValue: {} \
+                \n\tDate: {}'.format(self.name,
+                                    self.flowType.name, 
+                                    self.value,
+                                    self.date)
