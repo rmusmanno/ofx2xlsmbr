45e9786cf43ad5ce9f0756a51e17f9a9536b784c
Rafael Musmanno <rafa.musmanno@gmail.com>
2019-05-24 14:36:22 -0300

    Correcao na main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from model.BankStatement import BankStatement
 from model.CashFlow import CashFlow, CashFlowType
 import datetime
 
+# TODO: passar isso para uma unidade de testes. Manter a main enxuta
 outflow = [CashFlow('debit 1', CashFlowType.DEBIT, -100.0, datetime.datetime.now()),
             CashFlow('debit 2', CashFlowType.DEBIT, -127.0),
             CashFlow('credit 1', CashFlowType.CREDIT, -42.0)]
