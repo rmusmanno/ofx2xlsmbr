6c1c9fb6df04d4305be9fbe432acde9ed852b0d3
Rafael Musmanno <rafa.musmanno@gmail.com>
2019-05-27 14:32:18 -0300

    Acertando arquivo main

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from tests.FactoryTest import csvWriterControllerTest
 # TODO: passar isso para uma unidade de testes. Manter a main enxuta
 def main():
     print('main called')
-    pass
-
-if __name__ == "__main__":
     csvWriterControllerTest()
     #csvFactoryTest()
-    #main()
+
+if __name__ == "__main__":
+    main()
