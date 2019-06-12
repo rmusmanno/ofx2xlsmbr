4379e57ee816a7bf4c1e0de480449fa4626aa242
Rafael Musmanno <rafa.musmanno@gmail.com>
2019-06-12 17:33:36 -0300

    Consertando erros de compatibilidade Bradesco

--- reader/OFXReaderController.py
+++ reader/OFXReaderController.py
@@ -4,16 +4,30 @@ from model.BankStatement import BankStatement
 from model.CashFlow import CashFlow, CashFlowType
 
 import datetime
+from pytz import timezone
+import pytz
 
+import ofxtools
 from ofxtools import OFXTree
 
 class OFXReaderController(IReaderController):
     def read(self, factory, inputFilename) -> BankStatement:
         tree = OFXTree()
         tree.parse(inputFilename)
+
+        # erros de compatibilidade
+        self.treatBradescoException(tree)
+        
         convertedTree = tree.convert()
 
         bsReader = factory.createReaderBankStatement()
         bs = bsReader.read(factory, convertedTree)
 
         return bs
+
+    def treatBradescoException(self, tree):
+        root = tree.getroot()
+        dtServer = root.findall("SIGNONMSGSRSV1")[0].findall("SONRS")[0].findall("DTSERVER")[0]
+        if (int(dtServer.text) == 0):
+            unknownDateInThePast = datetime.datetime(1985, 10, 21, tzinfo=timezone('Brazil/East'))
+            dtServer.text = unknownDateInThePast
