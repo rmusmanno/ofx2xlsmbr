a67ddccbff6174c37adc4bbc99af8dc6f14d680b
Rafael Musmanno <rafa.musmanno@gmail.com>
2019-06-14 15:52:27 -0300

    Consertando hackfix de erro do bradesco

--- reader/OFXReaderController.py
+++ reader/OFXReaderController.py
@@ -28,6 +28,11 @@ class OFXReaderController(IReaderController):
     def treatBradescoException(self, tree):
         root = tree.getroot()
         dtServer = root.findall("SIGNONMSGSRSV1")[0].findall("SONRS")[0].findall("DTSERVER")[0]
-        if (int(dtServer.text) == 0):
-            unknownDateInThePast = datetime.datetime(1985, 10, 21, tzinfo=timezone('Brazil/East'))
-            dtServer.text = unknownDateInThePast
+        try:
+            if (int(dtServer.text) == 0):
+                unknownDateInThePast = datetime.datetime(1985, 10, 21, tzinfo=timezone('Brazil/East'))
+                dtServer.text = unknownDateInThePast
+        except ValueError:
+            pass
+            # se dtServer for um datetime, ele da erro na conversao para int
+            # print('Correcting DTServer')
