a666ecb5b0be994b10526789166756c5478759b7
Rafael Musmanno <rafa.musmanno@gmail.com>
2019-06-24 23:16:55 -0300

    Corrigindo url do repositorio

--- setup.py
+++ setup.py
@@ -8,23 +8,24 @@ setup(
     'ofx2xlsmbr.reader.xml',
     'ofx2xlsmbr.writer',
     'ofx2xlsmbr.writer.xlsm'],   # Chose the same as "name"
-  version = '0.72',      # Start with a small number and increase it with every change you make
+  version = '0.73',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Convert ofx to xlsm',   # Give a short description about your library
   author = 'Rafael Musmanno',                   # Type in your name
   author_email = 'rafa.musmanno@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/rmusmanno/ofx2xlsmbr/',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/rmusmanno/ofx2xlsmbr/archive/v0.71.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/rmusmanno/ofx2xlsmbr/archive/v0.72.tar.gz',    # I explain this later on
   keywords = ['ofx', 'xlsm'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
         'et-xmlfile',
         'jdcal',
         'openpyxl',
+        'ofxtools==0.0.1',
         'pytz',
         'lxml',
       ],
   dependency_links=[
-        "git+https://github.com/rmusmanno/ofxtools#egg=ofxtools"
+        "git+ssh://git@github.com/rmusmanno/ofxtools@0.0.1#egg=ofxtools-0.0.1"
     ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
