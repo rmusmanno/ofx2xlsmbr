6e7ff33812b7b662e02ea30ad884a3800a573f9e
Rafael Musmanno <rafa.musmanno@gmail.com>
2019-10-01 15:29:50 -0300

    Parse pdf itau

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 #from ofx2xlsmbr.tests.OFXReaderTest import ofxReaderTest, ofxReaderTestCartaoBradesco, ofxReaderTestCartaoBradescoDuplo, ofxReaderTestCartaoBradesco2, ofxReaderTestFile, ofxMultipleReaderTestFile
 #from ofx2xlsmbr.tests.XMLReaderTest import xmlReaderTestFile, xmlReaderTestFileCreditCard, ofxXmlReaderTestFile
 #from ofx2xlsmbr.tests.XLSReaderTest import xlsReaderTestFile, xlsReaderTestFile2, xlsReaderTestFile3, xlsMultipleReaderTestFile
-from ofx2xlsmbr.tests.ProsperarCoreTest import prosperarCoreTest, prosperarCoreTestBBCartao, prosperarCoreTestInputDuploOfx, prosperarCoreTestReturnBS, prosperarCoreTestOFXAndXLS, prosperarCoreTestDuplicateEntry, prosperarCoreTestReceiveBS
+from ofx2xlsmbr.tests.PDFReaderTest import pdfReaderTest
+#from ofx2xlsmbr.tests.ProsperarCoreTest import prosperarCoreTest, prosperarCoreTestBBCartao, prosperarCoreTestInputDuploOfx, prosperarCoreTestReturnBS, prosperarCoreTestOFXAndXLS, prosperarCoreTestDuplicateEntry, prosperarCoreTestReceiveBS
 #from ofx2xlsmbr.tests.BankStatementAdderTest import bankStatementAdderTest
 #from ofx2xlsmbr.controller.AppController import AppController
 
@@ -42,6 +43,7 @@ def runTests():
     #csvFactoryTest()
     #xlsWriterControllerTest()
     #xlsMultipleWriterControllerTest()
+    pdfReaderTest()
 
     #suite = unittest.TestLoader().loadTestsFromTestCase(BSWriterTest)
 
