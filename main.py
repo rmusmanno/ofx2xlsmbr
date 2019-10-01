013d6b4139d05d077e40a7974f3969b316a2a371
Rafael Musmanno <rafa.musmanno@gmail.com>
2019-10-01 18:28:19 -0300

    Atualizando versao

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #from ofx2xlsmbr.tests.CSVWriterTest import csvWriterControllerTest
-from ofx2xlsmbr.tests.XLSWriterTest import xlsWriterControllerTest, xlsMultipleWriterControllerTest
+#from ofx2xlsmbr.tests.XLSWriterTest import xlsWriterControllerTest, xlsMultipleWriterControllerTest
 #from ofx2xlsmbr.tests.OFXReaderTest import ofxReaderTest, ofxReaderTestCartaoBradesco, ofxReaderTestCartaoBradescoDuplo, ofxReaderTestCartaoBradesco2, ofxReaderTestFile, ofxMultipleReaderTestFile
 #from ofx2xlsmbr.tests.XMLReaderTest import xmlReaderTestFile, xmlReaderTestFileCreditCard, ofxXmlReaderTestFile
-#from ofx2xlsmbr.tests.XLSReaderTest import xlsReaderTestFile, xlsReaderTestFile2, xlsReaderTestFile3, xlsReaderTestFile_Origem, xlsMultipleReaderTestFile
+from ofx2xlsmbr.tests.XLSReaderTest import xlsReaderTestFile, xlsReaderTestFile2, xlsReaderTestFile3, xlsReaderTestFile_Origem, xlsMultipleReaderTestFile
 #from ofx2xlsmbr.tests.PDFReaderTest import pdfReaderTest
 #from ofx2xlsmbr.tests.ProsperarCoreTest import prosperarCoreTest, prosperarCoreTestBBCartao, prosperarCoreTestInputDuploOfx, prosperarCoreTestReturnBS, prosperarCoreTestOFXAndXLS, prosperarCoreTestDuplicateEntry, prosperarCoreTestReceiveBS
 #from ofx2xlsmbr.tests.BankStatementAdderTest import bankStatementAdderTest
@@ -38,7 +38,7 @@ def runTests():
     #xlsReaderTestFile()
     #xlsReaderTestFile2()
     #xlsReaderTestFile3()
-    #xlsReaderTestFile_Origem()
+    xlsReaderTestFile_Origem()
     #xlsMultipleReaderTestFile()
     #csvWriterControllerTest()
     #csvFactoryTest()
