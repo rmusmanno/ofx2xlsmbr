84380909eb0eaf90142d1623a9628a28dfaba75c
Rafael Musmanno <rafa.musmanno@gmail.com>
2019-10-01 17:22:56 -0300

    Modificando o escritor de XLS para estar de acordo com o leitor

--- ofx2xlsmbr/tests/XLSWriterTest.py
+++ ofx2xlsmbr/tests/XLSWriterTest.py
@@ -2,6 +2,9 @@ from ..factory.XLSMWriterFactory import XLSMWriterFactory
 
 from ..model.BankStatement import BankStatement
 from ..model.CashFlow import CashFlow, CashFlowType
+from ..model.Origin import Origin, AccountType
+
+from collections import namedtuple
 
 import datetime
 import logging
@@ -11,13 +14,23 @@ logger = logging.getLogger(__name__)
 def xlsWriterControllerTest():
     factory = XLSMWriterFactory()
 
-    outflow = [CashFlow('debit 1', CashFlowType.DEBIT, -100.0, datetime.datetime.now()),
-                CashFlow('debit 2', CashFlowType.DEBIT, -127.0),
-                CashFlow('credit 1', CashFlowType.CREDIT, -42.0)]
+    Account = namedtuple('Account', 'acctid')
+    account = Account(acctid = '1112213141')
+    origin_credit = Origin(account)
+    origin_credit.account_type = AccountType.CREDITCARD
 
-    inflow = [CashFlow('credit 1', CashFlowType.CREDIT, 1000.0)]
+    origin_debit = Origin(account)
+    origin_debit.account_type = AccountType.BANKACCOUNT
 
-    bs = BankStatement(inflow, outflow)
+    outflow = [CashFlow('debit 1', CashFlowType.DEBIT, -100.0, datetime.datetime.now(), origin=origin_debit),
+                CashFlow('debit 2', CashFlowType.DEBIT, -127.0, origin=origin_debit),
+                CashFlow('credit 1', CashFlowType.CREDIT, -42.0, origin=origin_credit)]
+
+    inflow = [CashFlow('credit 1', CashFlowType.CREDIT, 1000.0, origin=origin_credit)]
+
+    bs = BankStatement()
+    bs.inflows = inflow
+    bs.outflows = outflow
 
     writerController = factory.createWriterController()
     writerController.write([bs], factory, 'test')
