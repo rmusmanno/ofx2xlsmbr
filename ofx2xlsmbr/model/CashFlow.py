134265a423cfb206453cf87534b85333b0359da1
Silvino Vieira <silvino.vieira@finxi.com.br>
2019-10-07 16:57:31 -0300

    feat: Add cash date to CashFlow model
    https://projetok1.atlassian.net/browse/KN-177

--- ofx2xlsmbr/model/CashFlow.py
+++ ofx2xlsmbr/model/CashFlow.py
@@ -15,11 +15,13 @@ class CashFlow(object):
                  flowType: CashFlowType = CashFlowType.DEBIT,
                  value: float = 0.0,
                  date: str = 'N/A',
+                 cash_date: str = 'N/A',
                  origin: Origin = None):
         self.name = name
         self.flowType = flowType
         self.value = value
         self.date = date
+        self.cash_date = cash_date
         self.origin = origin
 
     def __eq__(self, other):
@@ -28,6 +30,7 @@ class CashFlow(object):
                 self.name.strip() == other.name.strip() and
                 self.value == other.value and
                 self.date == other.date and
+                self.cash_date == other.cash_date and
                 self.origin == other.origin
         )
 
@@ -37,10 +40,12 @@ class CashFlow(object):
                 \n\tType: {} \
                 \n\tValue: {} \
                 \n\tDate: {} \
+                \n\tCash date: {} \
                 \n\tOrigin: {}'.format(self.name,
                                        self.flowType.name,
                                        self.value,
                                        self.date,
+                                       self.cash_date,
                                        self.origin)
 
 
