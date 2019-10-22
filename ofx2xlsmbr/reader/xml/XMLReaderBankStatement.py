e8383afd0e1043a8cba408897bea4ee951735421
Silvino Vieira <silvino.vieira@finxi.com.br>
2019-10-22 16:50:31 -0300

    fix: Add origin to XML reader
    https://projetok1.atlassian.net/browse/FT-208

--- ofx2xlsmbr/reader/xml/XMLReaderBankStatement.py
+++ ofx2xlsmbr/reader/xml/XMLReaderBankStatement.py
@@ -1,5 +1,6 @@
 from ofx2xlsmbr.reader.IReaderBankStatement import IReaderBankStatement
 from ofx2xlsmbr.model.BankStatement import BankStatement
+from ofx2xlsmbr.model.Origin import Origin
 
 import xml.etree.ElementTree as ET
 
@@ -14,8 +15,28 @@ class XMLReaderBankStatement(IReaderBankStatement):
             if (options['creditcard'] == True):
                 #signalMultiplier = -1
                 tranList = ofx.find('CREDITCARDMSGSRSV1').find('CCSTMTTRNRS').find('CCSTMTRS')
+
+                # Origin
+                institution = None
+                branch = None
+                account_id = tranList.find('CCACCTFROM').find('ACCTID').text
+                account_type = 'CREDITCARD'
             else:
                 tranList = ofx.find('BANKMSGSRSV1').find('STMTTRNRS').find('STMTRS')
+
+                # Origin
+                account = tranList.find('BANKACCTFROM')
+                institution = account.find('BANKID').text
+                branch = account.find('BRANCHID').text
+                account_id = account.find('ACCTID').text
+                account_type = 'BANKACCOUNT'
+
+        origin = Origin(
+            account_id=account_id,
+            branch=branch,
+            institution=institution,
+            type=account_type,
+        )
 
         if (tranList):
             tranList = tranList.find('BANKTRANLIST')
@@ -25,6 +46,7 @@ class XMLReaderBankStatement(IReaderBankStatement):
         csReader = factory.createReaderCashFlow()
         for tx in txs:
             cs = csReader.read(factory, tx)
+            cs.origin = origin
             cs.value = float(cs.value)
             cs.value *= signalMultiplier
             if (cs.value >= float(0.0)):
